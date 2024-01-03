1a1576685388f6365f2196e34f19005558525b3f
VasaOne <derrien.maxime77@gmail.com>
2024-01-03 23:51:53 +0100

    basic working system for calculus module

--- calculus/calculus.py
+++ calculus/calculus.py
@@ -1,1 +1,25 @@
-start()
+from classes import *
+from datetime import datetime, timedelta
+#start()
+
+
+if __name__ == "__main__" :
+    #fake scenario creation
+    param = Parametre(datetime(2022, 1, 3), datetime(2022, 2, 5), True, False)
+    scenar = Scenario("test 1", param, 0.5)
+    
+    mat_1 = Materiel(0, 0, 1, 1)
+    mat_2 = Materiel(1, 0, 2, 1)
+    mat_3 = Materiel(2, 1, 1, 1)
+
+    centre_1 = Centre(1, "centre 1", [mat_1, mat_2]) 
+    centre_2 = Centre(2, "centre 2", [mat_3])
+
+    scenar.list_centre.append(centre_1)
+    scenar.list_centre.append(centre_2)
+    scenar.actualize()
+    print(len(scenar.list_centre))
+    bilan = Bilan("chemin_fichier")
+    bilan.printer(scenar)
+
+
--- calculus/classes.py
+++ calculus/classes.py
@@ -1,23 +1,39 @@
-NBR_CENTER = 0
-NBR_MATERIEL = 0
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
+DELTA = timedelta(hours = 1) #dela of one hour
 
 class Centre :
-    def __init__(self, name, mat_list):
-        self.id_centre = NBR_CENTER
-        ID_CENTER +=1
+    def __init__(self, id_centre, name, mat_list):
+        self.id_centre = id_centre
         self.name = name
         self.mat_list = mat_list.copy()
-        bilan_graph = [[][]]
+        self.bilan_graph = {"date" : [] , "value" : []}
+
+    def create_bilan(self, date_debut, date_fin):
+        date_actu = date_debut
+        while (date_actu < date_fin) :
+            date_actu += DELTA
+            self.bilan_graph["date"].append(date_actu)
+            conso_score = 0
+            for materiel in self.mat_list :
+                conso_score += materiel.get_cost(date_actu)
+            self.bilan_graph["value"].append(conso_score)
+
 
 
 class Materiel :
-    def __init__(self):
-        self.id = NBR_MATERIEL
-        NBR_MATERIEL +=1
+    def __init__(self,id_mat, id_centre, id_materiel, sous_id_materiel):
+        self.id = id_mat
         self.id_centre = id_centre
         self.id_materiel = id_materiel
         self.sous_id_materiel = sous_id_materiel
+        self.crea_cost = 0
+        self.end_cost = 0
+        
+    def get_cost(self, date) :
         #not finished
+        return 1
 
 class Communication :
     def __init__(self, id_com, centre_1, centre_2, requettes) :
@@ -35,19 +51,29 @@ class Parametre :
 
 class Scenario :
     def __init__(self, nom_scenario, parametre, kwTOco2):
+        self.nom = nom_scenario
         self.list_centre = []
         self.list_communication = []
         self.parametre = parametre
         self.kwTOco2 = kwTOco2
 
+    def actualize(self):
+        for centre in self.list_centre :
+            centre.create_bilan(self.parametre.date_debut,self.parametre.date_fin)
+        #TODO miss part for communication
+
 class Bilan :
     def __init__(self, chemin_fichier) :
         self.chemin_fichier = chemin_fichier
 
-    def printer(self):
-        print("bilan printer func")
+    def printer(self, scenario):
+        plt.title(scenario.nom)
+        plt.legend(title = "impacte environemental du système")
+        for centre in scenario.list_centre :
+            plt.plot(centre.bilan_graph["date"], centre.bilan_graph["value"], color = 'red', label = str(centre.id_centre))
+        plt.show()
 
-    def writer(self):
+    def writer(self, scenario):
         print("bilan writer func")
 
 
